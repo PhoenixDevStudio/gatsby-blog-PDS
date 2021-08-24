fc908c39882c68088953c1043a270a718a2d9698
PhoenixDevStudio <66450374+PhoenixDevStudio@users.noreply.github.com>
2021-08-23 21:00:51 -0500

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+require("dotenv").config()
+
+module.exports = {
+  siteMetadata: {
+    title: "My Super Cool Blog",
+  },
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+    
+    "gatsby-plugin-mdx",
+    
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require("./src/utils/algolia-queries")
+      },
+    }
+
+  ],
+};
